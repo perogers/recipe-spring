5081c3ae60fe8ccd265dc8836936bc69fd5fd32f
paulrogers <rogers.paul.work@gmail.com>
2017-12-05 18:26:15 -0500

    added domain objects
    completed domain objects

---
+++ src/main/java/com/rioverde/recipe/domain/Category.java
@@ -0,0 +1,41 @@
+package com.rioverde.recipe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    String description;
+
+    @ManyToMany(mappedBy = "categories")
+    Set<Recipe> recipes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
---
+++ src/main/java/com/rioverde/recipe/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.rioverde.recipe.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
