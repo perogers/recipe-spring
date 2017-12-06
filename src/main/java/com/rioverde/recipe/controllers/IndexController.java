068fd99243312583b7f8c1cf58f36178e3894cc8
paulrogers <rogers.paul.work@gmail.com>
2017-12-06 11:14:26 -0500

    Added query code for category & UoM

--- src/main/java/com/rioverde/recipe/controllers/IndexController.java
+++ src/main/java/com/rioverde/recipe/controllers/IndexController.java
@@ -1,12 +1,34 @@
 package com.rioverde.recipe.controllers;
 
+import com.rioverde.recipe.domain.Category;
+import com.rioverde.recipe.domain.UnitOfMeasure;
+import com.rioverde.recipe.respositories.CategoryRepository;
+import com.rioverde.recipe.respositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
     @RequestMapping({"", "/", "index"})
-        public String getIndexPage() {
-            return "index";
-        }
+    public String getIndexPage() {
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("Japanese");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findUnitOfMeasureByDescription("Teaspoon");
+
+        System.out.println("Category: " + optionalCategory.get().toString());
+        System.out.println("Unit of Measure: " + optionalUnitOfMeasure.get().toString());
+
+        return "index";
+    }
 }
--- src/main/java/com/rioverde/recipe/respositories/UnitOfMeasureRepository.java
+++ src/main/java/com/rioverde/recipe/respositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.rioverde.recipe.respositories;
 import com.rioverde.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>    {
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findUnitOfMeasureByDescription(String description);
 }
