646b590616e94e49db28acf588665d8cd133a614
paulrogers <rogers.paul.work@gmail.com>
2017-12-08 12:14:59 -0500

    Added Lombok annotations and log config file

--- src/main/java/com/rioverde/recipe/domain/Notes.java
+++ src/main/java/com/rioverde/recipe/domain/Notes.java
@@ -1,7 +1,11 @@
 package com.rioverde.recipe.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
@@ -22,27 +26,4 @@ public class Notes {
         this.recipeNotes = recipeNotes;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
--- src/main/java/com/rioverde/recipe/services/RecipeServiceImpl.java
+++ src/main/java/com/rioverde/recipe/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.rioverde.recipe.services;
 
 import com.rioverde.recipe.domain.Recipe;
 import com.rioverde.recipe.respositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("Hi");
+
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
 
